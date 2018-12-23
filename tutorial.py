84e41d00c3d4caf4c09241ab339b38a381298ec5
wendyliong <w.wend85@gmail.com>
2018-12-23 20:24:05 +0700

    My Django Girls app, first commit

---
+++ tutorial.py
@@ -0,0 +1,18 @@
+if 5 > 2 :
+	print("5 is greater than 2")
+else :
+	print("5 is not greater than 2")
+
+name = "Wendy"
+
+def hi(name):
+	#print("Hi There")
+	#print("How are you?")
+	if name == "Ola":
+		print("Hi Ola!")
+	elif name == "Sonja":
+		print("Hi Sonja!")
+	else:
+		print("Hi Anonymous")
+
+hi(name)	
